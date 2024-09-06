59c9d44c2046877ebc7dbd928be8412af086fe55
elv-zenia <zenia.villa@eluv.io>
2024-09-06 09:09:23 -0700

    Remove loading results text

--- src/pages/search/clips-grid/ClipsGrid.jsx
+++ src/pages/search/clips-grid/ClipsGrid.jsx
@@ -98,8 +98,6 @@ const ClipsGrid = observer(({clips, song, view="HIGH_SCORE"}) => {
 
   const filteredClips = clips.filter(item => view === "ALL" ? true : parseInt(item._score || "") >= 60);
 
-  if(searchStore.currentSearch.terms && filteredClips.length === 0) { return "No results"; }
-
   return (
     <SimpleGrid cols={4} spacing="lg">
       {
