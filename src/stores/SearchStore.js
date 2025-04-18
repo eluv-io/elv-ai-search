98d8bb2645fca859cc7bf3846fc7457292e199db
elv-zenia <zenia.villa@eluv.io>
2025-04-18 10:49:05 -0700

    Change default search host to ai-02

--- src/stores/SearchStore.js
+++ src/stores/SearchStore.js
@@ -13,7 +13,7 @@ class SearchStore {
   };
 
   customIndex = "";
-  searchHostname = "ai";
+  searchHostname = "ai-02";
   searchSummaryType = "synopsis"; // synopsis, caption, caption2
   searchContentType; // ALL, IMAGES, VIDEOS
   resultsViewType; // Show all results vs results that have a high score.
